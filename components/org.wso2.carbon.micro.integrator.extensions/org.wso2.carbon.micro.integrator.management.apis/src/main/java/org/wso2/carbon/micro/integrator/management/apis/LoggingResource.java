c9fd7951dcb1883a257f5a66b3364fc9243d885a
SanojPunchihewa <sanoj.punchihewa@gmail.com>
2019-06-05 14:54:38 +0530

    Add method to update system logs

--- components/org.wso2.carbon.micro.integrator.extensions/org.wso2.carbon.micro.integrator.management.apis/src/main/java/org/wso2/carbon/micro/integrator/management/apis/LoggingResource.java
+++ components/org.wso2.carbon.micro.integrator.extensions/org.wso2.carbon.micro.integrator.management.apis/src/main/java/org/wso2/carbon/micro/integrator/management/apis/LoggingResource.java
@@ -1,13 +1,21 @@
 package org.wso2.carbon.micro.integrator.management.apis;
 
+import org.apache.log4j.Appender;
+import org.apache.log4j.Level;
+import org.apache.log4j.LogManager;
+import org.apache.log4j.Logger;
 import org.apache.synapse.MessageContext;
+import org.apache.synapse.commons.json.JsonUtil;
 import org.apache.synapse.core.axis2.Axis2MessageContext;
+import org.json.JSONObject;
 
+import java.util.Enumeration;
 import java.util.HashSet;
-import java.util.Objects;
 import java.util.Set;
 
 public class LoggingResource extends ApiResource {
+
+    private static String[] logLevels = new String[]{"OFF", "TRACE", "DEBUG", "INFO", "WARN", "ERROR", "FATAL"};
 
     public LoggingResource(String urlTemplate) {
         super(urlTemplate);
@@ -18,7 +26,7 @@ public class LoggingResource extends ApiResource {
 
         Set<String> methods = new HashSet<>();
         methods.add("GET");
-        methods.add("POST");
+        methods.add("PATCH");
         return methods;
     }
 
@@ -27,10 +35,81 @@ public class LoggingResource extends ApiResource {
 
         buildMessage(messageContext);
 
-        String requestPayload = messageContext.getEnvelope().getBody().toString();
+        org.apache.axis2.context.MessageContext axis2MessageContext =
+                ((Axis2MessageContext) messageContext).getAxis2MessageContext();
 
-        // axis2MessageContext.removeProperty(Constants.NO_ENTITY_BODY);
+        JSONObject jsonPayload = new JSONObject(JsonUtil.jsonPayloadToString(axis2MessageContext));
+
+        String logLevel = jsonPayload.getString("loggingLevel");
+        boolean persist = jsonPayload.getBoolean("persist");
+
+        setSystemLog(messageContext, logLevel, persist);
+
+        axis2MessageContext.removeProperty(Constants.NO_ENTITY_BODY);
         return true;
     }
 
+    private void setSystemLog(MessageContext messageContext, String logLevel, boolean persist) {
+
+        org.apache.axis2.context.MessageContext axis2MessageContext =
+                ((Axis2MessageContext) messageContext).getAxis2MessageContext();
+
+        JSONObject jsonBody = updateSystemLog(logLevel, persist);
+
+        Utils.setJsonPayLoad(axis2MessageContext, jsonBody);
+    }
+
+    private JSONObject updateSystemLog(String logLevel, boolean persist) {
+
+        JSONObject jsonObject = new JSONObject();
+
+        if (!isALogLevel(logLevel)) {
+            jsonObject.put("message", "Invalid log level");
+            return jsonObject;
+        }
+
+        Set<Appender> appenderSet = new HashSet<>();
+
+        if (persist) {
+            //
+        }
+
+        // update root logger details
+        Logger rootLogger = Logger.getRootLogger();
+        rootLogger.setLevel(Level.toLevel(logLevel));
+        addAppendersToSet(rootLogger.getAllAppenders(), appenderSet);
+
+        // update logger and appender data, following are set
+        // 1. log level of all the loggers to logLevel
+        // 2. pattern of all the appenders to logpattern
+        Enumeration loggersEnum = LogManager.getCurrentLoggers();
+        Level systemLevel = Level.toLevel(logLevel);
+        while (loggersEnum.hasMoreElements()) {
+            Logger logger = (Logger) loggersEnum.nextElement();
+            // we ignore all class level defined loggers
+            addAppendersToSet(logger.getAllAppenders(), appenderSet);
+            logger.setLevel(systemLevel);
+        }
+        jsonObject.put("message", "Successfully updated");
+        return jsonObject;
+    }
+
+    private void addAppendersToSet(Enumeration appenders, Set<Appender> appenderSet) {
+        Appender appender;
+        while (appenders.hasMoreElements()) {
+            appender = (Appender) appenders.nextElement();
+            if (appender.getName() != null) {
+                appenderSet.add(appender);
+            }
+        }
+    }
+
+    private boolean isALogLevel(String logLevelToTest) {
+        boolean returnValue = false;
+        for (String logLevel : logLevels) {
+            if (logLevel.equalsIgnoreCase(logLevelToTest))
+                returnValue = true;
+        }
+        return returnValue;
+    }
 }
