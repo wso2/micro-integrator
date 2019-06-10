8eac12d70b91a771380e0d93f146d825577730dc
Nirothipan <nirothipan@gmail.com>
2019-06-10 11:36:30 +0530

    Fix FaultDBExceptionTestCase to work with MI

--- integration/dataservice-hosting-tests/tests-integration/tests/src/test/java/org/wso2/ei/dataservice/integration/test/faulty/service/FaultDBExceptionTestCase.java
+++ integration/dataservice-hosting-tests/tests-integration/tests/src/test/java/org/wso2/ei/dataservice/integration/test/faulty/service/FaultDBExceptionTestCase.java
@@ -20,24 +20,16 @@ package org.wso2.ei.dataservice.integration.test.faulty.service;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
-import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 import org.wso2.carbon.dataservices.samples.fault_dataservice.DataServiceFaultException;
 import org.wso2.carbon.dataservices.samples.fault_dataservice.FaultDBServiceStub;
 import org.wso2.ei.dataservice.integration.test.DSSIntegrationTest;
-import org.wso2.ei.dataservices.integration.common.clients.DataServiceFileUploaderClient;
 
-import java.io.File;
-import java.net.URL;
 import java.rmi.RemoteException;
-import javax.activation.DataHandler;
-
-import static org.testng.Assert.assertTrue;
 
 public class FaultDBExceptionTestCase extends DSSIntegrationTest {
 
-    private String serviceName = "FaultDBService";
     private String serverEpr;
 
     private static final Log log = LogFactory.getLog(FaultDBExceptionTestCase.class);
@@ -45,25 +37,12 @@ public class FaultDBExceptionTestCase extends DSSIntegrationTest {
     @BeforeClass(alwaysRun = true)
     public void initialize() throws Exception {
         super.init();
-        String resourceFileLocation = null;
+        String serviceName = "FaultDBService";
         serverEpr = getServiceUrlHttp(serviceName);
-        resourceFileLocation = getResourceLocation();
-        DataServiceFileUploaderClient dataServiceAdminClient = new DataServiceFileUploaderClient(
-                dssContext.getContextUrls().getBackEndUrl(), sessionCookie);
-        dataServiceAdminClient.uploadDataServiceFile("FaultDBService.dbs", new DataHandler(
-                new URL("file:///" + resourceFileLocation + File.separator + "samples" + File.separator + "dbs"
-                        + File.separator + "rdbms" + File.separator + "FaultDBService.dbs")));
-        log.info(serviceName + " uploaded");
-    }
-
-    @Test(groups = "wso2.dss", description = "Check whether fault service deployed or not")
-    public void testServiceDeployment() throws Exception {
-        assertTrue(isServiceDeployed(serviceName));
-        log.info(serviceName + " is deployed");
     }
 
     @Test(groups = { "wso2.dss" }, description = "Checking the Exception returned from DSS when Database/table"
-            + " does not exist for the data service", dependsOnMethods = "testServiceDeployment")
+            + " does not exist for the data service")
     public void testExceptionForUnavailableDB() throws RemoteException {
         log.info("Running faultServiceTestCase#testExceptionForUnavailableDB");
         FaultDBServiceStub faultDBServiceStub = new FaultDBServiceStub(serverEpr);
@@ -75,8 +54,4 @@ public class FaultDBExceptionTestCase extends DSSIntegrationTest {
         }
     }
 
-    @AfterClass(alwaysRun = true, groups = "wso2.dss", description = "delete service")
-    public void deleteFaultyService() throws Exception {
-        deleteService(serviceName);
-    }
 }
