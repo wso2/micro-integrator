b5f630a036a20df594c4c2c52f1bb93e615ddf57
Nirothipan <nirothipan@gmail.com>
2019-06-10 14:23:16 +0530

    Fix WhiteSpaceWithQueryParamsTest to work with MI

--- integration/dataservice-hosting-tests/tests-integration/tests/src/test/java/org/wso2/ei/dataservice/integration/test/syntax/WhiteSpaceWithQueryParamsTest.java
+++ integration/dataservice-hosting-tests/tests-integration/tests/src/test/java/org/wso2/ei/dataservice/integration/test/syntax/WhiteSpaceWithQueryParamsTest.java
@@ -26,18 +26,10 @@ import org.apache.axis2.AxisFault;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.testng.Assert;
-import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 import org.wso2.carbon.automation.test.utils.axis2client.AxisServiceClient;
-import org.wso2.ei.dataservice.integration.common.utils.DSSTestCaseUtils;
 import org.wso2.ei.dataservice.integration.test.DSSIntegrationTest;
-
-import java.io.File;
-import java.util.ArrayList;
-import java.util.List;
-
-import static org.testng.Assert.assertTrue;
 
 //JIRA  https://wso2.org/jira/browse/CARBON-11321
 public class WhiteSpaceWithQueryParamsTest extends DSSIntegrationTest {
@@ -47,29 +39,16 @@ public class WhiteSpaceWithQueryParamsTest extends DSSIntegrationTest {
     private final OMFactory fac = OMAbstractFactory.getOMFactory();
     private final OMNamespace omNs = fac
             .createOMNamespace("http://ws.wso2.org/dataservice/samples/rdbms_sample", "ns1");
-    private static String serviceName = "WhiteSpacesInQueryParamsTest";
     private static String serviceEndPoint;
-    private DSSTestCaseUtils dssTest;
 
     @BeforeClass(alwaysRun = true)
     public void initialize() throws Exception {
         super.init();
-        List<File> sqlFileLis = new ArrayList<File>();
-        dssTest = new DSSTestCaseUtils();
-
-        sqlFileLis.add(selectSqlFile("CreateTables.sql"));
-        deployService(serviceName, createArtifact(
-                getResourceLocation() + File.separator + "dbs" + File.separator + "rdbms" + File.separator + "MySql"
-                        + File.separator + "WhiteSpacesInQueryParamsTest.dbs", sqlFileLis));
+        String serviceName = "WhiteSpacesInQueryParamsTest";
         serviceEndPoint = getServiceUrlHttp(serviceName);
     }
 
-    @Test(groups = "wso2.dss", description = "check whether the service is deployed")
-    public void testServiceDeployment() throws Exception {
-        assertTrue(dssTest.isServiceDeployed(dssContext.getContextUrls().getBackEndUrl(), sessionCookie, serviceName));
-    }
-
-    @Test(groups = "wso2.dss", dependsOnMethods = { "testServiceDeployment" }, description = "add employee records")
+    @Test(groups = "wso2.dss", description = "add employee records")
     public void testInsertOperation() throws AxisFault {
         for (int i = 0; i < 5; i++) {
             addEmployee(serviceEndPoint, String.valueOf(i));
@@ -92,11 +71,6 @@ public class WhiteSpaceWithQueryParamsTest extends DSSIntegrationTest {
             verifyDeletion(String.valueOf(i));
         }
         log.info("Delete operation success");
-    }
-
-    @AfterClass(alwaysRun = true)
-    public void testCleanup() throws Exception {
-        dssTest.deleteService(dssContext.getContextUrls().getBackEndUrl(), sessionCookie, serviceName);
     }
 
     private void addEmployee(String serviceEndPoint, String employeeNumber) throws AxisFault {
@@ -126,7 +100,7 @@ public class WhiteSpaceWithQueryParamsTest extends DSSIntegrationTest {
 
     }
 
-    private OMElement getEmployeeById(String employeeNumber) throws AxisFault {
+    private void getEmployeeById(String employeeNumber) throws AxisFault {
         OMElement payload = fac.createOMElement("employeesByNumber", omNs);
 
         OMElement empNo = fac.createOMElement("employeeNumber", omNs);
@@ -135,7 +109,6 @@ public class WhiteSpaceWithQueryParamsTest extends DSSIntegrationTest {
 
         OMElement result = new AxisServiceClient().sendReceive(payload, serviceEndPoint, "employeesByNumber");
         Assert.assertTrue(result.toString().contains("<first-name>AAA</first-name>"), "Expected Result Mismatched");
-        return result;
     }
 
     private void deleteEmployeeById(String employeeNumber) throws AxisFault {
