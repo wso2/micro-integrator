d954ed7b33f3026df4c9b8d20e0edb747fbfaf6d
Nirothipan <nirothipan@gmail.com>
2019-06-10 11:52:05 +0530

    Fix InvalidClosingTagUnDeployedServiceTestCase to work with MI

--- integration/dataservice-hosting-tests/tests-integration/tests/src/test/java/org/wso2/ei/dataservice/integration/test/faulty/service/InvalidClosingTagUnDeployedServiceTestCase.java
+++ integration/dataservice-hosting-tests/tests-integration/tests/src/test/java/org/wso2/ei/dataservice/integration/test/faulty/service/InvalidClosingTagUnDeployedServiceTestCase.java
@@ -18,73 +18,29 @@
 
 package org.wso2.ei.dataservice.integration.test.faulty.service;
 
-import org.apache.axiom.attachments.ByteArrayDataSource;
 import org.apache.axis2.AxisFault;
-import org.apache.commons.logging.Log;
-import org.apache.commons.logging.LogFactory;
 import org.testng.Assert;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
-import org.wso2.carbon.automation.test.utils.common.FileManager;
-import org.wso2.carbon.dataservices.ui.fileupload.stub.ExceptionException;
 import org.wso2.ei.dataservice.integration.test.DSSIntegrationTest;
-import org.wso2.ei.dataservices.integration.common.clients.DataServiceFileUploaderClient;
+import org.wso2.esb.integration.common.utils.clients.axis2client.AxisServiceClient;
 
-import java.io.File;
 import java.rmi.RemoteException;
-import java.util.ArrayList;
-import javax.activation.DataHandler;
 import javax.xml.xpath.XPathExpressionException;
 
-import static org.testng.Assert.assertFalse;
-
 public class InvalidClosingTagUnDeployedServiceTestCase extends DSSIntegrationTest {
-    private static final Log log = LogFactory.getLog(InvalidClosingTagUnDeployedServiceTestCase.class);
-
-    private final String serviceName = "FaultyDataService";
-    private final String serviceFile = "FaultyDataService.dbs";
-    private String resourceFileLocation;
-    private DataServiceFileUploaderClient dataServiceAdminClient;
-    private DataHandler dhArtifact;
 
     @BeforeClass(alwaysRun = true)
     public void serviceDeployment() throws Exception {
         super.init();
-        resourceFileLocation = getResourceLocation();
-        String serviceFilePath =
-                resourceFileLocation + File.separator + "dbs" + File.separator + "rdbms" + File.separator + "MySql"
-                        + File.separator + serviceFile;
-
-        createArtifact(serviceFilePath, getSqlScript());
-        dataServiceAdminClient = new DataServiceFileUploaderClient(dssContext.getContextUrls().getBackEndUrl(),
-                sessionCookie);
-
-        ByteArrayDataSource dbs;
-        String content = FileManager.readFile(serviceFilePath);
-        Assert.assertTrue(content.contains("</query>"), "query tag missing");
-        content = content.replaceFirst("</query>", "</que>");
-        dbs = new ByteArrayDataSource(content.getBytes());
-        dhArtifact = new DataHandler(dbs);
-
     }
 
     @Test(groups = "wso2.dss", description = "deploy invalid dbs", expectedExceptions = AxisFault.class)
-    public void testDeployService() throws ExceptionException, RemoteException {
-        Assert.assertTrue(dataServiceAdminClient.uploadDataServiceFile(serviceFile, dhArtifact),
-                "Service Deployment Failed while uploading service file");
+    public void testDeployService() throws RemoteException, XPathExpressionException {
 
+        String serviceName = "FaultyDataService";
+        new AxisServiceClient().sendReceive(null, getServiceUrlHttp(serviceName), "select_all_Customers_operation");
+        Assert.fail();
     }
 
-    @Test(groups = "wso2.dss", description = "Check whether fault service deployed or not", dependsOnMethods = "testDeployService")
-    public void isServiceDeployed() throws Exception {
-        assertFalse(isServiceDeployed(serviceName));
-        log.info(serviceName + " is faulty");
-    }
-
-    private ArrayList<File> getSqlScript() throws XPathExpressionException {
-        ArrayList<File> al = new ArrayList<File>();
-        al.add(selectSqlFile("CreateTables.sql"));
-        al.add(selectSqlFile("Offices.sql"));
-        return al;
-    }
 }
