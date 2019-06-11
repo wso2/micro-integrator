962e96c78ef9918c2f01286fded2506c179a6b7c
Nirothipan <nirothipan@gmail.com>
2019-06-11 14:43:00 +0530

    Fix RestFulServiceTestCase to work with MI

--- integration/dataservice-hosting-tests/tests-integration/tests/src/test/java/org/wso2/ei/dataservice/integration/test/services/RestFulServiceTestCase.java
+++ integration/dataservice-hosting-tests/tests-integration/tests/src/test/java/org/wso2/ei/dataservice/integration/test/services/RestFulServiceTestCase.java
@@ -17,42 +17,31 @@
  */
 package org.wso2.ei.dataservice.integration.test.services;
 
+import org.apache.axiom.om.OMAbstractFactory;
 import org.apache.axiom.om.OMElement;
+import org.apache.axiom.om.OMFactory;
+import org.apache.axiom.om.OMNamespace;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.testng.Assert;
-import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
-import org.wso2.carbon.automation.test.utils.http.client.HttpClientUtil;
+import org.wso2.carbon.automation.extensions.servers.httpserver.SimpleHttpClient;
 import org.wso2.ei.dataservice.integration.test.DSSIntegrationTest;
 
-import java.io.File;
-import java.util.ArrayList;
-import java.util.List;
+import java.util.HashMap;
+import java.util.Map;
 
 public class RestFulServiceTestCase extends DSSIntegrationTest {
     private static final Log log = LogFactory.getLog(RestFulServiceTestCase.class);
-    private final String serviceName = "ResourcesServiceTest";
     private String serviceEndPoint;
 
     @BeforeClass(alwaysRun = true)
     public void serviceDeployment() throws Exception {
 
         super.init();
-        List<File> sqlFileLis = new ArrayList<File>();
-        sqlFileLis.add(selectSqlFile("CreateTables.sql"));
-        deployService(serviceName, createArtifact(
-                getResourceLocation() + File.separator + "dbs" + File.separator + "rdbms" + File.separator + "MySql"
-                        + File.separator + "ResourcesServiceTest.dbs", sqlFileLis));
+        String serviceName = "ResourcesServiceTest";
         serviceEndPoint = getServiceUrlHttp(serviceName) + "/";
-
-    }
-
-    @AfterClass(alwaysRun = true)
-    public void destroy() throws Exception {
-        deleteService(serviceName);
-        cleanup();
     }
 
     @Test(groups = { "wso2.dss" })
@@ -69,13 +58,12 @@ public class RestFulServiceTestCase extends DSSIntegrationTest {
 
     @Test(groups = { "wso2.dss" }, dependsOnMethods = { "postRequest" })
     public void getRequestWithParam() throws Exception {
-        OMElement response;
+        String response;
         for (int i = 1; i < 6; i++) {
             response = getProductByCode(i + "");
-            Assert.assertTrue(response.toString().contains("<productName>product" + i + "</productName>"),
+            Assert.assertTrue(response.contains("<productName>product" + i + "</productName>"),
                     "Expected result not found");
-            Assert.assertTrue(response.toString().contains("<productLine>2</productLine>"),
-                    "Expected result not found");
+            Assert.assertTrue(response.contains("<productLine>2</productLine>"), "Expected result not found");
         }
         log.info("GET Request with parameter verified");
     }
@@ -83,12 +71,12 @@ public class RestFulServiceTestCase extends DSSIntegrationTest {
     @Test(groups = { "wso2.dss" }, dependsOnMethods = { "postRequest" })
     public void putRequest() throws Exception {
         editProduct();
-        OMElement response;
+        String response;
         for (int i = 1; i < 6; i++) {
             response = getProductByCode(i + "");
-            Assert.assertTrue(response.toString().contains("<productName>product" + i + " edited</productName>"),
+            Assert.assertTrue(response.contains("<productName>product" + i + " edited</productName>"),
                     "Expected result not found");
-            Assert.assertTrue(response.toString().contains("<buyPrice>15.0</buyPrice>"), "Expected result not found");
+            Assert.assertTrue(response.contains("<buyPrice>15.0</buyPrice>"), "Expected result not found");
         }
         log.info("PUT Request verified");
     }
@@ -101,58 +89,86 @@ public class RestFulServiceTestCase extends DSSIntegrationTest {
 
     private void deleteProduct() throws Exception {
 
-        HttpClientUtil httpClient = new HttpClientUtil();
         for (int i = 1; i < 6; i++) {
-            httpClient.delete(serviceEndPoint + "product/", "" + i);
-
+            SimpleHttpClient client = new SimpleHttpClient();
+            String endpoint = serviceEndPoint + "product/" + i;
+            client.doDelete(endpoint, null);
         }
-
     }
 
     private void listProduct() throws Exception {
 
-        HttpClientUtil httpClient = new HttpClientUtil();
-        OMElement result = httpClient.get(serviceEndPoint + "_getproducts");
+        SimpleHttpClient client = new SimpleHttpClient();
+        Map<String, String> headers = new HashMap<>();
+        headers.put("Accept", "application/xml");
+        String endpoint = serviceEndPoint + "products";
+        String result = client.getResponsePayload(client.doGet(endpoint, headers));
         Assert.assertNotNull(result, "Response null");
         for (int i = 1; i < 6; i++) {
-            Assert.assertTrue(result.toString().contains("<productCode>" + i + "</productCode>"),
-                    "Expected result not found");
+            Assert.assertTrue(result.contains("<productCode>" + i + "</productCode>"), "Expected result not found");
         }
-
     }
 
     private void editProduct() throws Exception {
 
-        HttpClientUtil httpClient = new HttpClientUtil();
         for (int i = 1; i < 6; i++) {
-
-            String para =
-                    "productCode=" + i + "&" + "productName=" + "product" + i + " edited" + "&" + "productLine=2" + "&"
-                            + "quantityInStock=200" + "&" + "buyPrice=15";
-            httpClient.put(serviceEndPoint + "_putproduct", para);
-
+            SimpleHttpClient client = new SimpleHttpClient();
+            Map<String, String> headers = new HashMap<>();
+            headers.put("Content-Type", "application/xml");
+            String endpoint = serviceEndPoint + "product";
+            client.doPut(endpoint, headers, getPayload(i, " edited", "15.0").toString(), "application/xml");
         }
-
     }
 
-    private OMElement getProductByCode(String productId) throws Exception {
+    private String getProductByCode(String productId) throws Exception {
 
-        HttpClientUtil httpClient = new HttpClientUtil();
-
-        return httpClient.get(serviceEndPoint + "product/" + productId);
-
+        SimpleHttpClient client = new SimpleHttpClient();
+        Map<String, String> headers = new HashMap<>();
+        headers.put("Accept", "application/xml");
+        String endpoint = serviceEndPoint + "product/" + productId;
+        org.apache.http.HttpResponse response = client.doGet(endpoint, headers);
+        return client.getResponsePayload(response);
     }
 
     private void addProduct() throws Exception {
 
-        HttpClientUtil httpClient = new HttpClientUtil();
         for (int i = 1; i < 6; i++) {
-
-            String para = "productCode=" + i + "&" + "productName=" + "product" + i + "&" + "productLine=2" + "&"
-                    + "quantityInStock=200" + "&" + "buyPrice=10";
-            httpClient.post(serviceEndPoint + "_postproduct", para);
-
+            SimpleHttpClient client = new SimpleHttpClient();
+            Map<String, String> headers = new HashMap<>();
+            headers.put("Content-Type", "application/xml");
+            String endpoint = serviceEndPoint + "product";
+            client.doPost(endpoint, headers, getPayload(i, "", "10").toString(), "application/xml");
         }
+    }
+
+    private OMElement getPayload(int i, String status, String price) {
+
+        OMFactory fac = OMAbstractFactory.getOMFactory();
+        OMNamespace p = fac.createOMNamespace("http://ws.wso2.org/dataservice/samples/resources_sample", "p");
+        OMNamespace xs = fac.createOMNamespace("http://ws.wso2.org/dataservice/samples/resources_sample", "xs");
+        OMElement payload = fac.createOMElement("_postproduct", p);
+
+        OMElement id = fac.createOMElement("productCode", xs);
+        id.setText(Integer.toString(i));
+        payload.addChild(id);
+
+        id = fac.createOMElement("productName", xs);
+        id.setText("product" + i + status);
+        payload.addChild(id);
+
+        id = fac.createOMElement("productLine", xs);
+        id.setText("2");
+        payload.addChild(id);
+
+        id = fac.createOMElement("quantityInStock", xs);
+        id.setText("200");
+        payload.addChild(id);
+
+        id = fac.createOMElement("buyPrice", xs);
+        id.setText(price);
+        payload.addChild(id);
+
+        return payload;
     }
 
 }
