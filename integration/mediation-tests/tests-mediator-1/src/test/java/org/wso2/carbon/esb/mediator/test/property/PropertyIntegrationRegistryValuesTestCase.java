2a3028dcf2208d7f1e5acc38c832e24aa50ef035
sajithaliyanage <sajithaliyanage@gmail.com>
2019-06-20 19:18:52 +0530

    fix test:PropertyIntegrationRegistryValuesTestCase

--- integration/mediation-tests/tests-mediator-1/src/test/java/org/wso2/carbon/esb/mediator/test/property/PropertyIntegrationRegistryValuesTestCase.java
+++ integration/mediation-tests/tests-mediator-1/src/test/java/org/wso2/carbon/esb/mediator/test/property/PropertyIntegrationRegistryValuesTestCase.java
@@ -1,14 +1,9 @@
 package org.wso2.carbon.esb.mediator.test.property;
 
 import org.apache.axiom.om.OMElement;
-import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
-import org.wso2.esb.integration.common.clients.registry.PropertiesAdminServiceClient;
-import org.wso2.esb.integration.common.clients.registry.ResourceAdminServiceClient;
 import org.wso2.esb.integration.common.utils.ESBIntegrationTest;
-
-import javax.activation.DataHandler;
 
 import static org.testng.Assert.assertTrue;
 
@@ -18,46 +13,9 @@ import static org.testng.Assert.assertTrue;
  */
 public class PropertyIntegrationRegistryValuesTestCase extends ESBIntegrationTest {
 
-    private static final String CONF_PATH = "/_system/config/custom";
-    private static final String GOV_PATH = "/_system/governance/custom";
-
     @BeforeClass(alwaysRun = true)
     public void setEnvironment() throws Exception {
         super.init();
-        uploadResourcesToRegistry();
-    }
-
-    /**
-     * This method uploads the resources to the registry
-     *
-     * @throws Exception
-     */
-    private void uploadResourcesToRegistry() throws Exception {
-        ResourceAdminServiceClient resourceAdminServiceClient = new ResourceAdminServiceClient(
-                contextUrls.getBackEndUrl(), getSessionCookie());
-        PropertiesAdminServiceClient propertiesAdminServiceClient = new PropertiesAdminServiceClient(
-                contextUrls.getBackEndUrl(), getSessionCookie());
-
-        resourceAdminServiceClient.deleteResource("/_system/config/custom");
-        resourceAdminServiceClient.deleteResource("/_system/governance/custom");
-
-        resourceAdminServiceClient.addCollection(CONF_PATH, "test", "", "Contains property mediator test resources");
-
-        resourceAdminServiceClient.addCollection(GOV_PATH, "test", "", "Contains property mediator test resources");
-
-        resourceAdminServiceClient
-                .addResource(CONF_PATH + "/test/property_mediator_test.txt", "text/plain", "text files",
-                        new DataHandler("Property mediator test resources".getBytes(), "application/text"));
-        propertiesAdminServiceClient
-                .setProperty(CONF_PATH + "/test/property_mediator_test.txt", "resourceName", "Config Reg Test String");
-
-        resourceAdminServiceClient
-                .addResource(GOV_PATH + "/test/property_mediator_test.txt", "text/plain", "text files",
-                        new DataHandler("Property mediator test resources".getBytes(), "application/text"));
-        propertiesAdminServiceClient
-                .setProperty(GOV_PATH + "/test/property_mediator_test.txt", "resourceName", "Gov Reg Test String");
-
-        Thread.sleep(1000);
     }
 
     @Test(groups = "wso2.esb", description = "Set value from config registry (default scope)")
@@ -74,24 +32,5 @@ public class PropertyIntegrationRegistryValuesTestCase extends ESBIntegrationTes
                 .sendSimpleStockQuoteRequest(getProxyServiceURLHttp("propertyGovRegistryTestProxy"), null,
                         "Random Symbol");
         assertTrue(response.toString().contains("Gov Reg Test String"), "Property Not Set");
-    }
-
-    private void clearRegistry() throws Exception {
-        ResourceAdminServiceClient resourceAdminServiceClient = new ResourceAdminServiceClient(
-                contextUrls.getBackEndUrl(), getSessionCookie());
-
-        resourceAdminServiceClient.deleteResource("/_system/config/custom/test");
-
-        resourceAdminServiceClient.deleteResource("/_system/governance/custom/test");
-    }
-
-    @AfterClass(alwaysRun = true)
-    private void destroy() throws Exception {
-        try {
-            clearRegistry();
-        } finally {
-            super.cleanup();
-        }
-
     }
 }
