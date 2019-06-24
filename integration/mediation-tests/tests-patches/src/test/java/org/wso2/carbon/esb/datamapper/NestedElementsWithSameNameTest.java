81057dda921fb6254ac2fa2e3d6a277fe30091fb
GDLMadushanka <gdlmadushanka@gmail.com>
2019-06-24 15:32:48 +0530

    Fix test-patches test cases
    Fix NestedElementsWithSameNameTest and FaultSequenceExecutionOrderTestCase

--- integration/mediation-tests/tests-patches/src/test/java/org/wso2/carbon/esb/datamapper/NestedElementsWithSameNameTest.java
+++ integration/mediation-tests/tests-patches/src/test/java/org/wso2/carbon/esb/datamapper/NestedElementsWithSameNameTest.java
@@ -22,21 +22,13 @@ import org.testng.Assert;
 import org.testng.annotations.Test;
 import org.wso2.carbon.esb.datamapper.common.DataMapperIntegrationTest;
 
-import java.io.File;
-
 /**
  * Test cases for github.com/wso2/product-ei/issues/2391
  */
 public class NestedElementsWithSameNameTest extends DataMapperIntegrationTest {
 
-    private final String DM_ARTIFACT_ROOT_PATH = "/artifacts/ESB/mediatorconfig/datamapper/nestedElements/";
-    private final String DM_REGISTRY_ROOT_PATH = "datamapper/";
-
-    @Test(groups = { "wso2.esb" }, description = "Datamapper : test nested elements with same name")
+    @Test(groups = {"wso2.esb"}, description = "Datamapper : test nested elements with same name")
     public void testNestedElementsWithSameName() throws Exception {
-        loadESBConfigurationFromClasspath(DM_ARTIFACT_ROOT_PATH + File.separator + "synapse.xml");
-        uploadResourcesToGovernanceRegistry(DM_REGISTRY_ROOT_PATH, DM_ARTIFACT_ROOT_PATH, "NestedElementConfig.dmc",
-                "NestedElementConfig_inputSchema.json", "NestedElementConfig_outputSchema.json");
 
         String requestMsg =
                 "{\n" + "    \"chartfield\": [ { \n" + "        \"chartfield\": true\n" + "    } ] \n" + "}";
