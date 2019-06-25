14d98eb5e7e4e4be508c7128bbcd1d5f835187f1
GDLMadushanka <gdlmadushanka@gmail.com>
2019-06-25 16:52:21 +0530

    Fix passthrough test cases
    Fix PickEndPointFromRegistryTestCase
    InSequenceInlineEndpointFromRegistryTestCase
    WSDLOptionsPickedFromRegistryTestCase
    InvokingNonExistingProxyTestCase

--- integration/mediation-tests/tests-service/src/test/java/org/wso2/carbon/esb/proxyservice/test/loggingProxy/WSDLOptionsPickedFromRegistryTestCase.java
+++ integration/mediation-tests/tests-service/src/test/java/org/wso2/carbon/esb/proxyservice/test/loggingProxy/WSDLOptionsPickedFromRegistryTestCase.java
@@ -18,18 +18,11 @@
 package org.wso2.carbon.esb.proxyservice.test.loggingProxy;
 
 import org.apache.axiom.om.OMElement;
-import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
-import org.wso2.carbon.registry.resource.stub.ResourceAdminServiceExceptionException;
-import org.wso2.esb.integration.common.clients.registry.ResourceAdminServiceClient;
 import org.wso2.esb.integration.common.utils.ESBIntegrationTest;
 
-import java.net.URL;
-import java.rmi.RemoteException;
-import javax.activation.DataHandler;
 import javax.xml.namespace.QName;
-import javax.xml.xpath.XPathExpressionException;
 
 import static org.testng.Assert.assertEquals;
 import static org.testng.Assert.assertNotNull;
@@ -39,11 +32,6 @@ public class WSDLOptionsPickedFromRegistryTestCase extends ESBIntegrationTest {
     @BeforeClass(alwaysRun = true)
     public void setEnvironment() throws Exception {
         super.init();
-        uploadResourcesToConfigRegistry();
-        loadESBConfigurationFromClasspath(
-                "/artifacts/ESB/proxyconfig/proxy/loggingProxy/wsdl_options_pick_from_registry.xml");
-        isProxyDeployed("wsdlOptionsFromRegLoggingProxy");
-
     }
 
     @Test(groups = "wso2.esb", description = "- Logging proxy" + "- Publish WSDL Options - Picked from registry")
@@ -63,41 +51,7 @@ public class WSDLOptionsPickedFromRegistryTestCase extends ESBIntegrationTest {
     }
 
     @Test(groups = "wso2.esb", description = "- Logging proxy -Publish WSDL Options - Picked from registry -Log", enabled = false)
-    public void testLoggingProxyLogging() throws Exception {
+    public void testLoggingProxyLogging() {
         //ToDo Assert Logs
-    }
-
-    @AfterClass(alwaysRun = true)
-    public void destroy() throws Exception, InterruptedException, RemoteException {
-        clearUploadedResource();
-        super.cleanup();
-    }
-
-    private void uploadResourcesToConfigRegistry() throws Exception {
-
-        ResourceAdminServiceClient resourceAdminServiceStub = new ResourceAdminServiceClient(
-                context.getContextUrls().getBackEndUrl(), getSessionCookie());
-
-        resourceAdminServiceStub.deleteResource("/_system/config/proxy");
-        resourceAdminServiceStub.addCollection("/_system/config/", "proxy", "", "Contains test proxy tests files");
-
-        resourceAdminServiceStub
-                .addResource("/_system/config/proxy/sample_proxy_1.wsdl", "application/wsdl+xml", "wsdl+xml files",
-                        new DataHandler(new URL("file:///" + getESBResourceLocation()
-                                + "/proxyconfig/proxy/utils/sample_proxy_1.wsdl")));
-
-        Thread.sleep(1000);
-
-    }
-
-    private void clearUploadedResource()
-            throws InterruptedException, ResourceAdminServiceExceptionException, RemoteException,
-            XPathExpressionException {
-
-        ResourceAdminServiceClient resourceAdminServiceStub = new ResourceAdminServiceClient(
-                context.getContextUrls().getBackEndUrl(), getSessionCookie());
-
-        resourceAdminServiceStub.deleteResource("/_system/config/proxy");
-
     }
 }
