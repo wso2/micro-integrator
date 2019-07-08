02b2d7fa5e75cf9b1b5b7d23b389edd33ceb1b5e
SachithK <sachithk@wso2.com>
2019-07-08 11:19:18 +0530

    Add synapse xml configs to api and proxy response XML configuration of proxies and apies added when monitoring them via mgt api

--- components/org.wso2.carbon.micro.integrator.extensions/org.wso2.carbon.micro.integrator.management.apis/src/main/java/org/wso2/carbon/micro/integrator/management/apis/Constants.java
+++ components/org.wso2.carbon.micro.integrator.extensions/org.wso2.carbon.micro.integrator.management.apis/src/main/java/org/wso2/carbon/micro/integrator/management/apis/Constants.java
@@ -66,4 +66,6 @@ public class Constants {
 
     public static final String HEADER_VALUE_APPLICATION_JSON = "application/json";
     public static final String MESSAGE_JSON_ATTRIBUTE = "Message";
+
+    public static final String SYNAPSE_CONFIGURATION = "configuration";
 }
