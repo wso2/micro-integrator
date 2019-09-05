ec11b6ff89fef802a5bf1cf73fdbf018844ff04a
Nirothipan <nirothipan@gmail.com>
2019-09-05 11:39:18 +0530

    Format code

--- components/mediation/tasks/org.wso2.micro.integrator.mediation.ntask/src/main/java/org/wso2/micro/integrator/mediation/ntask/Constants.java
+++ components/mediation/tasks/org.wso2.micro.integrator.mediation.ntask/src/main/java/org/wso2/micro/integrator/mediation/ntask/Constants.java
@@ -19,16 +19,10 @@
 package org.wso2.micro.integrator.mediation.ntask;
 
 public class Constants {
-    public static final String TASK_TYPE_ESB = "ESB_TASK";
-    public static final String TASK_EXECUTING_TENANT_ID = "CURRENT_TASK_EXECUTING_TENANT_IDENTIFIER";
-    /**
-     * Parameter name which has clustering pattern
-     */
-    public static final String CLUSTERING_PATTERN = "clusteringPattern";
-    /**
-     * worker manager clustering pattern
-     */
-    public static final String CLUSTERING_PATTERN_WORKER_MANAGER = "WorkerManager";
+
+    static final String TASK_TYPE_ESB = "ESB_TASK";
+    static final String TASK_EXECUTING_TENANT_ID = "CURRENT_TASK_EXECUTING_TENANT_IDENTIFIER";
+
 
     static final int SUPER_TENANT_ID = -1234;
 }
