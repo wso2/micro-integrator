4f94d5aa3b417fefd9014ae2212607ebbfb53f79
JanakaPushpakumara <bajpushpakumara@gmail.com>
2019-09-18 16:49:45 +0530

    Adding the pox security axis2 module and fix the UTOverTransport security policy scenario

---
+++ components/mediation/extensions/org.wso2.micro.integrator.security.pox/src/org/wso2/micro/integrator/security/pox/SecurityConstants.java
@@ -0,0 +1,5 @@
+package org.wso2.micro.integrator.security.pox;
+
+public class SecurityConstants {
+    public static final String USERNAME_TOKEN_SCENARIO_ID = "scenario1";
+}
---
+++ components/org.wso2.micro.integrator.pox.security/src/main/java/org/wso2/micro/integrator/security/utils/SecurityTokenStore.java
@@ -0,0 +1,6 @@
+package org.wso2.micro.integrator.security.utils;
+
+import org.apache.rahas.SimpleTokenStore;
+
+public class SecurityTokenStore extends SimpleTokenStore{
+}
