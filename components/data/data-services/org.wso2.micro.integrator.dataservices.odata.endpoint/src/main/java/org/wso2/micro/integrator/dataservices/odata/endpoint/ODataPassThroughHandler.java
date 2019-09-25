a4728c35ffcf9e683baa2d270fbd2a2964e22d4b
Chanika Geeganage <chanika@Chanikas-MacBook-Pro.local>
2019-09-25 18:36:31 +0530

    Format code

--- components/data/data-services/org.wso2.micro.integrator.dataservices.odata.endpoint/src/main/java/org/wso2/micro/integrator/dataservices/odata/endpoint/ODataPassThroughHandler.java
+++ components/data/data-services/org.wso2.micro.integrator.dataservices.odata.endpoint/src/main/java/org/wso2/micro/integrator/dataservices/odata/endpoint/ODataPassThroughHandler.java
@@ -43,87 +43,87 @@ import java.io.UnsupportedEncodingException;
 import java.util.Map;
 
 public class ODataPassThroughHandler extends AbstractSynapseHandler {
-	private static final Log log = LogFactory.getLog(ODataPassThroughHandler.class);
-	public final static String JSON_CONTENT_TYPE = "application/json";
-	public final static String XML_CONTENT_TYPE  = "application/xml";
+    private static final Log log = LogFactory.getLog(ODataPassThroughHandler.class);
+    public final static String JSON_CONTENT_TYPE = "application/json";
+    public final static String XML_CONTENT_TYPE = "application/xml";
 
 
-	@Override
-	public boolean handleRequestInFlow(MessageContext messageContext) {
-		try {
-			org.apache.axis2.context.MessageContext axis2MessageContext =
-					((Axis2MessageContext) messageContext).getAxis2MessageContext();
-			Object isODataService = axis2MessageContext.getProperty("IsODataService");
-			// In this if block we are skipping proxy services, inbound related message contexts & api.
-			if (axis2MessageContext.getProperty("TransportInURL") != null && isODataService != null ) {
-				RelayUtils.buildMessage(axis2MessageContext);
-				ODataServletRequest request = new ODataServletRequest(axis2MessageContext);
-				ODataServletResponse response = new ODataServletResponse(axis2MessageContext);
-				ODataEndpoint.process(request, response);
-				setContent(axis2MessageContext, response);
-				setHeaders(axis2MessageContext, response);
-				messageContext.setTo(null);
-				messageContext.setResponse(true);
-				Axis2Sender.sendBack(messageContext);
-			}
-			return true;
-		} catch (Exception e) {
-			this.handleException("Error occurred in integrator handler.", e, messageContext);
-			return true;
-		}
-	}
+    @Override
+    public boolean handleRequestInFlow(MessageContext messageContext) {
+        try {
+            org.apache.axis2.context.MessageContext axis2MessageContext =
+                    ((Axis2MessageContext) messageContext).getAxis2MessageContext();
+            Object isODataService = axis2MessageContext.getProperty("IsODataService");
+            // In this if block we are skipping proxy services, inbound related message contexts & api.
+            if (axis2MessageContext.getProperty("TransportInURL") != null && isODataService != null) {
+                RelayUtils.buildMessage(axis2MessageContext);
+                ODataServletRequest request = new ODataServletRequest(axis2MessageContext);
+                ODataServletResponse response = new ODataServletResponse(axis2MessageContext);
+                ODataEndpoint.process(request, response);
+                setContent(axis2MessageContext, response);
+                setHeaders(axis2MessageContext, response);
+                messageContext.setTo(null);
+                messageContext.setResponse(true);
+                Axis2Sender.sendBack(messageContext);
+            }
+            return true;
+        } catch (Exception e) {
+            this.handleException("Error occurred in integrator handler.", e, messageContext);
+            return true;
+        }
+    }
 
-	private void setHeaders(org.apache.axis2.context.MessageContext axis2MessageContext,
-							 ODataServletResponse response) throws UnsupportedEncodingException {
-		axis2MessageContext.setProperty(Constants.Configuration.CONTENT_TYPE, response.getContentType());
-		Object o = axis2MessageContext.getProperty(org.apache.axis2.context.MessageContext.TRANSPORT_HEADERS);
-		Map headers = (Map) o;
-		if (headers != null) {
-			headers.remove(HTTP.CONTENT_TYPE);
-			headers.put(HTTP.CONTENT_TYPE, response.getContentType());
-		}
-		if (response.getContentAsString() != null && response.getContentAsString().length() != 0) {
-			axis2MessageContext.removeProperty(PassThroughConstants.NO_ENTITY_BODY);
-		}
-	}
+    private void setHeaders(org.apache.axis2.context.MessageContext axis2MessageContext,
+                            ODataServletResponse response) throws UnsupportedEncodingException {
+        axis2MessageContext.setProperty(Constants.Configuration.CONTENT_TYPE, response.getContentType());
+        Object o = axis2MessageContext.getProperty(org.apache.axis2.context.MessageContext.TRANSPORT_HEADERS);
+        Map headers = (Map) o;
+        if (headers != null) {
+            headers.remove(HTTP.CONTENT_TYPE);
+            headers.put(HTTP.CONTENT_TYPE, response.getContentType());
+        }
+        if (response.getContentAsString() != null && response.getContentAsString().length() != 0) {
+            axis2MessageContext.removeProperty(PassThroughConstants.NO_ENTITY_BODY);
+        }
+    }
 
-	private void setContent(org.apache.axis2.context.MessageContext axis2MessageContext, ODataServletResponse response)
-			throws UnsupportedEncodingException, AxisFault, XMLStreamException {
-		String content = response.getContentAsString();
-		if (response.getContentType() != null && response.getContentType().contains(JSON_CONTENT_TYPE)) {
-			axis2MessageContext.setProperty(Constants.Configuration.MESSAGE_TYPE, JSON_CONTENT_TYPE);
-			JsonUtil.getNewJsonPayload(axis2MessageContext, content, true, true);
-		} else if (response.getContentType() != null && response.getContentType().contains(XML_CONTENT_TYPE)) {
-			axis2MessageContext.setProperty(Constants.Configuration.MESSAGE_TYPE, response.getContentType());
-			OMElement omXML = AXIOMUtil.stringToOM(content);
-			SOAPFactory fac = OMAbstractFactory.getSOAP11Factory();
-			SOAPEnvelope envelope = fac.getDefaultEnvelope();
-			envelope.getBody().addChild(omXML.getFirstElement());
-			axis2MessageContext.setEnvelope(envelope);
-		}
-	}
+    private void setContent(org.apache.axis2.context.MessageContext axis2MessageContext, ODataServletResponse response)
+            throws UnsupportedEncodingException, AxisFault, XMLStreamException {
+        String content = response.getContentAsString();
+        if (response.getContentType() != null && response.getContentType().contains(JSON_CONTENT_TYPE)) {
+            axis2MessageContext.setProperty(Constants.Configuration.MESSAGE_TYPE, JSON_CONTENT_TYPE);
+            JsonUtil.getNewJsonPayload(axis2MessageContext, content, true, true);
+        } else if (response.getContentType() != null && response.getContentType().contains(XML_CONTENT_TYPE)) {
+            axis2MessageContext.setProperty(Constants.Configuration.MESSAGE_TYPE, response.getContentType());
+            OMElement omXML = AXIOMUtil.stringToOM(content);
+            SOAPFactory fac = OMAbstractFactory.getSOAP11Factory();
+            SOAPEnvelope envelope = fac.getDefaultEnvelope();
+            envelope.getBody().addChild(omXML.getFirstElement());
+            axis2MessageContext.setEnvelope(envelope);
+        }
+    }
 
-	private void handleException(String msg, Exception e, MessageContext msgContext) {
-		log.error(msg, e);
-		if (msgContext.getServiceLog() != null) {
-			msgContext.getServiceLog().error(msg, e);
-		}
-		throw new SynapseException(msg, e);
-	}
+    private void handleException(String msg, Exception e, MessageContext msgContext) {
+        log.error(msg, e);
+        if (msgContext.getServiceLog() != null) {
+            msgContext.getServiceLog().error(msg, e);
+        }
+        throw new SynapseException(msg, e);
+    }
 
-	@Override
-	public boolean handleRequestOutFlow(MessageContext messageContext) {
-		return true;
-	}
+    @Override
+    public boolean handleRequestOutFlow(MessageContext messageContext) {
+        return true;
+    }
 
-	@Override
-	public boolean handleResponseInFlow(MessageContext messageContext) {
-		return true;
-	}
+    @Override
+    public boolean handleResponseInFlow(MessageContext messageContext) {
+        return true;
+    }
 
-	@Override
-	public boolean handleResponseOutFlow(MessageContext messageContext) {
-		return true;
-	}
+    @Override
+    public boolean handleResponseOutFlow(MessageContext messageContext) {
+        return true;
+    }
 
 }
