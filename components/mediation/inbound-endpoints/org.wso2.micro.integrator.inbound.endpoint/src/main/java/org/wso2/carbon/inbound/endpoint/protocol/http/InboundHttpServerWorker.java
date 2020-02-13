ef1f75b1012a9cbda9d9304a74e61b15e7a9ce84
Nirothipan <nirothipan@gmail.com>
2020-02-13 17:19:44 +0530

    Enable correlation log support for inbound endpoints
    Fixes https://github.com/wso2/micro-integrator/issues/1129

--- components/mediation/inbound-endpoints/org.wso2.micro.integrator.inbound.endpoint/src/main/java/org/wso2/carbon/inbound/endpoint/protocol/http/InboundHttpServerWorker.java
+++ components/mediation/inbound-endpoints/org.wso2.micro.integrator.inbound.endpoint/src/main/java/org/wso2/carbon/inbound/endpoint/protocol/http/InboundHttpServerWorker.java
@@ -60,7 +60,7 @@ public class InboundHttpServerWorker extends ServerWorker {
 
     private static final Log log = LogFactory.getLog(InboundHttpServerWorker.class);
 
-    private SourceRequest request = null;
+    private SourceRequest request;
     private int port;
     private String tenantDomain;
     private RESTRequestHandler restHandler;
@@ -121,9 +121,6 @@ public class InboundHttpServerWorker extends ServerWorker {
                     log.error("Cannot find deployed inbound endpoint " + endpointName + "for process request");
                     return;
                 }
-
-                //                OpenEventCollector.reportEntryEvent(synCtx, endpointName, endpoint.getAspectConfiguration(),
-                //                                                    ComponentType.INBOUNDENDPOINT);
 
                 CustomLogSetter.getInstance().setLogAppender(endpoint.getArtifactContainerName());
 
