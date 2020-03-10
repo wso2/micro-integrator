50ae40c08a37f6a3f8a142f87de4e9414169beec
Vathsan <srivathsanmail@gmail.com>
2020-03-10 15:22:46 +0530

    Fix codacy issues

--- distribution/src/docker-distribution/filtered/docker-entrypoint.sh
+++ distribution/src/docker-distribution/filtered/docker-entrypoint.sh
@@ -20,10 +20,10 @@
 set -e
 
 # check if the WSO2 non-root user home exists
-test ! -d ${WORKING_DIRECTORY} && echo "WSO2 Docker non-root user home does not exist" && exit 1
+test ! -d "${WORKING_DIRECTORY}" && echo "WSO2 Docker non-root user home does not exist" && exit 1
 
 # check if the WSO2 product home exists
-test ! -d ${WSO2_SERVER_HOME} && echo "WSO2 Docker product home does not exist" && exit 1
+test ! -d "${WSO2_SERVER_HOME}" && echo "WSO2 Docker product home does not exist" && exit 1
 
-# start WSO2 server
-sh ${WSO2_SERVER_HOME}/bin/micro-integrator.sh "$@"
+# start Micro Integrator
+sh "${WSO2_SERVER_HOME}"/bin/micro-integrator.sh "$@"
