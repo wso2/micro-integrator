58d74fb1c6cb773abc2b566f4616104a7d05e0c8
Nirothipan <nirothipan@gmail.com>
2020-03-20 14:59:07 +0530

    Make trigerring unresponsive events immediate

--- components/org.wso2.micro.integrator.coordination/src/main/java/org/wso2/micro/integrator/coordination/RDBMSMemberEventProcessor.java
+++ components/org.wso2.micro.integrator.coordination/src/main/java/org/wso2/micro/integrator/coordination/RDBMSMemberEventProcessor.java
@@ -84,8 +84,7 @@ public class RDBMSMemberEventProcessor {
                 scheduledPeriod = RDBMSConstantUtils.DEFAULT_SCHEDULED_PERIOD_INTERVAL;
             }
         }
-        membershipListenerTask = new RDBMSMemberEventListenerTask(nodeId, localGroupId, heartbeatMaxRetry,
-                                                                  communicationBusContext);
+        membershipListenerTask = new RDBMSMemberEventListenerTask(nodeId, localGroupId, communicationBusContext);
         this.clusterMembershipReaderTaskScheduler.scheduleWithFixedDelay(membershipListenerTask,
                                                                          scheduledPeriod, scheduledPeriod, TimeUnit.MILLISECONDS);
         if (log.isDebugEnabled()) {
