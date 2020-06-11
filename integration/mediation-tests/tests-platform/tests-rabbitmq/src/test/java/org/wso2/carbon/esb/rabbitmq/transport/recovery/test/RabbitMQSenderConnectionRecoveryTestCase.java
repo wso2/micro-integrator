4711c1a5c66d5e040ac7ebee05de661f5cb53782
sdkottegoda <sdkottegoda@gmail.com>
2020-06-11 05:42:07 +0530

    Enable connection recovery tests

--- integration/mediation-tests/tests-platform/tests-rabbitmq/src/test/java/org/wso2/carbon/esb/rabbitmq/transport/recovery/test/RabbitMQSenderConnectionRecoveryTestCase.java
+++ integration/mediation-tests/tests-platform/tests-rabbitmq/src/test/java/org/wso2/carbon/esb/rabbitmq/transport/recovery/test/RabbitMQSenderConnectionRecoveryTestCase.java
@@ -21,53 +21,31 @@ import org.testng.Assert;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
-import org.wso2.carbon.automation.engine.context.AutomationContext;
-import org.wso2.carbon.automation.engine.context.TestUserMode;
 import org.wso2.carbon.esb.rabbitmq.utils.RabbitMQTestUtils;
 import org.wso2.esb.integration.common.utils.ESBIntegrationTest;
 import org.wso2.esb.integration.common.utils.Utils;
 import org.wso2.esb.integration.common.utils.clients.axis2client.AxisServiceClient;
 import org.wso2.esb.integration.common.utils.clients.rabbitmqclient.RabbitMQConsumerClient;
-import org.wso2.esb.integration.common.utils.common.ServerConfigurationManager;
-import org.wso2.esb.integration.common.utils.servers.RabbitMQServer;
 
-import java.io.File;
 import java.io.IOException;
 import java.util.List;
 
 public class RabbitMQSenderConnectionRecoveryTestCase extends ESBIntegrationTest {
 
-    private RabbitMQServer rabbitMQServer;
-    private ServerConfigurationManager configurationManagerAxis2;
-
     @BeforeClass(alwaysRun = true)
     public void init() throws Exception {
         super.init();
-
-        rabbitMQServer = RabbitMQTestUtils.getRabbitMQServerInstance();
-
-        configurationManagerAxis2 = new ServerConfigurationManager(
-                new AutomationContext("ESB", TestUserMode.SUPER_TENANT_ADMIN));
-        File customAxisConfigAxis2 = new File(
-                getESBResourceLocation() + File.separator + "axis2config" + File.separator + "axis2.xml");
-        configurationManagerAxis2.applyConfiguration(customAxisConfigAxis2);
-        super.init();
-
-        loadESBConfigurationFromClasspath("/artifacts/ESB/rabbitmq/transport/rabbitmq_endpoint_proxy.xml");
-
     }
 
-    @Test(groups = { "wso2.esb" }, description = "Test ESB as a RabbitMQ sender with connection recovery")
+    @Test(groups = {"wso2.esb"}, description = "Test ESB as a RabbitMQ sender with connection recovery")
     public void testRabbitMQSenderRecoverySuccess() throws Exception {
-
-        rabbitMQServer.start();
-        rabbitMQServer.initialize();
 
         AxisServiceClient client = new AxisServiceClient();
         RabbitMQConsumerClient consumer = new RabbitMQConsumerClient("localhost");
 
+        String queue = "testRabbitMQSenderRecoverySuccess";
         try {
-            consumer.declareAndConnect("exchange1", "queue1");
+            consumer.declareAndConnect("exchange1", queue);
         } catch (IOException e) {
             Assert.fail("Could not connect to RabbitMQ broker");
         }
@@ -75,9 +53,10 @@ public class RabbitMQSenderConnectionRecoveryTestCase extends ESBIntegrationTest
         List<String> messages = consumer.popAllMessages();
         int beforeMessageCount = messages.size();
 
+        String proxyName = "RabbitMQSenderConnectionRecoveryTestProxy";
         for (int i = 0; i < 5; i++) {
-            client.sendRobust(Utils.getStockQuoteRequest("RMQ"), getProxyServiceURLHttp("RabbitMQProducerProxy"),
-                    "getQuote");
+            client.sendRobust(Utils.getStockQuoteRequest("RMQ"),
+                              getProxyServiceURLHttp(proxyName), "getQuote");
         }
 
         messages = consumer.popAllMessages();
@@ -87,26 +66,27 @@ public class RabbitMQSenderConnectionRecoveryTestCase extends ESBIntegrationTest
             Assert.fail("Messages not received at RabbitMQ Broker");
         } else {
             Assert.assertEquals(afterMessagesCount - beforeMessageCount, 5,
-                    "New messages not received at RabbitMQBroker");
+                                "New messages not received at RabbitMQBroker");
             for (int i = beforeMessageCount; i < afterMessagesCount; i++) {
                 Assert.assertNotNull(messages.get(i),
-                        "Message not found. message sent by proxy service not reached to the destination Queue");
+                                     "Message not found. message sent by proxy service not reached to the destination"
+                                     + " Queue");
                 Assert.assertTrue(messages.get(i).contains("<ns:getQuote xmlns:ns=\"http://services.samples\"><"
-                        + "ns:request><ns:symbol>RMQ</ns:symbol></ns:request></ns:getQuote>"), "Message mismatched");
+                                                           + "ns:request><ns:symbol>RMQ</ns:symbol></ns:request></ns"
+                                                           + ":getQuote>"), "Message mismatched");
             }
         }
 
         Thread.sleep(10000);
-        rabbitMQServer.stop();
+        RabbitMQTestUtils.stopRabbitMq();
 
         Thread.sleep(10000);
-        rabbitMQServer.start();
-        rabbitMQServer.initialize();
+        RabbitMQTestUtils.startRabbitMq();
 
         Thread.sleep(10000);
 
         try {
-            consumer.declareAndConnect("exchange1", "queue1");
+            consumer.declareAndConnect("exchange1", queue);
         } catch (IOException e) {
             Assert.fail("Could not connect to RabbitMQ broker");
         }
@@ -115,8 +95,8 @@ public class RabbitMQSenderConnectionRecoveryTestCase extends ESBIntegrationTest
         beforeMessageCount = messages.size();
 
         for (int i = 0; i < 5; i++) {
-            client.sendRobust(Utils.getStockQuoteRequest("RMQ"), getProxyServiceURLHttp("RabbitMQProducerProxy"),
-                    "getQuote");
+            client.sendRobust(Utils.getStockQuoteRequest("RMQ"), getProxyServiceURLHttp(proxyName),
+                              "getQuote");
         }
         Thread.sleep(10000);
 
@@ -127,24 +107,22 @@ public class RabbitMQSenderConnectionRecoveryTestCase extends ESBIntegrationTest
             Assert.fail("Messages not received at RabbitMQ Broker");
         } else {
             Assert.assertEquals(afterMessagesCount - beforeMessageCount, 5,
-                    "New messages not received at RabbitMQBroker");
+                                "New messages not received at RabbitMQBroker");
             for (int i = beforeMessageCount; i < afterMessagesCount; i++) {
                 Assert.assertNotNull(messages.get(i),
-                        "Message not found. message sent by proxy service not reached to the destination Queue");
+                                     "Message not found. message sent by proxy service not reached to the destination"
+                                     + " Queue");
                 Assert.assertTrue(messages.get(i).contains("<ns:getQuote xmlns:ns=\"http://services.samples\"><"
-                        + "ns:request><ns:symbol>RMQ</ns:symbol></ns:request></ns:getQuote>"), "Message mismatched");
+                                                           + "ns:request><ns:symbol>RMQ</ns:symbol></ns:request></ns"
+                                                           + ":getQuote>"), "Message mismatched");
             }
         }
 
         consumer.disconnect();
-        rabbitMQServer.stop();
     }
 
     @AfterClass(alwaysRun = true)
     public void end() throws Exception {
         super.cleanup();
-        rabbitMQServer.stop();
-        rabbitMQServer = null;
-        configurationManagerAxis2.restoreToLastConfiguration();
     }
 }
