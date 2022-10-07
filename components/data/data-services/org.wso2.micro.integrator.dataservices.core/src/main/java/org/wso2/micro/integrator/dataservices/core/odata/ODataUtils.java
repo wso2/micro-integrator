59f61033f3dfb4843d89ba395acfff7c929f2cd7
Pasan Tennakoon <pasan96tennakoon@gmail.com>
2022-10-07 14:50:51 +0530

    Add entity streaming support

--- components/data/data-services/org.wso2.micro.integrator.dataservices.core/src/main/java/org/wso2/micro/integrator/dataservices/core/odata/ODataUtils.java
+++ components/data/data-services/org.wso2.micro.integrator.dataservices.core/src/main/java/org/wso2/micro/integrator/dataservices/core/odata/ODataUtils.java
@@ -18,7 +18,13 @@
 
 package org.wso2.micro.integrator.dataservices.core.odata;
 
+import java.sql.Types;
+import java.text.ParseException;
+import org.apache.axis2.databinding.utils.ConverterUtil;
+import org.apache.commons.codec.binary.Base64;
 import org.apache.olingo.commons.api.data.Entity;
+import org.apache.olingo.commons.api.data.Property;
+import org.apache.olingo.commons.api.data.ValueType;
 import org.apache.olingo.commons.api.edm.EdmEntityType;
 import org.apache.olingo.commons.api.edm.EdmPrimitiveTypeException;
 import org.apache.olingo.commons.api.edm.EdmPrimitiveTypeKind;
@@ -29,6 +35,7 @@ import org.apache.olingo.commons.core.edm.primitivetype.EdmString;
 
 import java.util.Iterator;
 import java.util.UUID;
+import org.wso2.micro.integrator.dataservices.common.DBConstants;
 
 /**
  * Utility class for OData.
@@ -100,6 +107,180 @@ public class ODataUtils {
 
         location.append(")");
         return location.toString();
+    }
+
+    /**
+     * This method creates primitive type property.
+     *
+     * @param columnType Data type of the column - java.sql.Types
+     * @param name       Name of the column
+     * @param paramValue String value
+     * @return Property
+     * @throws ODataServiceFault
+     * @see Types
+     * @see Property
+     */
+    public static Property createPrimitive(final DataColumn.ODataDataType columnType, final String name,
+                                           final String paramValue) throws ODataServiceFault, ParseException {
+        String propertyType;
+        Object value;
+        switch (columnType) {
+            case INT32:
+                propertyType = EdmPrimitiveTypeKind.Int32.getFullQualifiedName().getFullQualifiedNameAsString();
+                value = paramValue == null ? null : ConverterUtil.convertToInt(paramValue);
+                break;
+            case INT16:
+                propertyType = EdmPrimitiveTypeKind.Int16.getFullQualifiedName().getFullQualifiedNameAsString();
+                value = paramValue == null ? null : ConverterUtil.convertToShort(paramValue);
+                break;
+            case DOUBLE:
+                propertyType = EdmPrimitiveTypeKind.Double.getFullQualifiedName().getFullQualifiedNameAsString();
+                value = paramValue == null ? null : ConverterUtil.convertToDouble(paramValue);
+                break;
+            case STRING:
+                propertyType = EdmPrimitiveTypeKind.String.getFullQualifiedName().getFullQualifiedNameAsString();
+                value = paramValue;
+                break;
+            case BOOLEAN:
+                propertyType = EdmPrimitiveTypeKind.Boolean.getFullQualifiedName().getFullQualifiedNameAsString();
+                value = paramValue == null ? null : ConverterUtil.convertToBoolean(paramValue);
+                break;
+            case BINARY:
+                propertyType = EdmPrimitiveTypeKind.Binary.getFullQualifiedName().getFullQualifiedNameAsString();
+                value = paramValue == null ? null : getBytesFromBase64String(paramValue);
+                break;
+            case BYTE:
+                propertyType = EdmPrimitiveTypeKind.Byte.getFullQualifiedName().getFullQualifiedNameAsString();
+                value = paramValue;
+                break;
+            case SBYTE:
+                propertyType = EdmPrimitiveTypeKind.SByte.getFullQualifiedName().getFullQualifiedNameAsString();
+                value = paramValue;
+                break;
+            case DATE:
+                propertyType = EdmPrimitiveTypeKind.Date.getFullQualifiedName().getFullQualifiedNameAsString();
+                value = ConverterUtil.convertToDate(paramValue);
+                break;
+            case DURATION:
+                propertyType = EdmPrimitiveTypeKind.Duration.getFullQualifiedName().getFullQualifiedNameAsString();
+                value = paramValue;
+                break;
+            case DECIMAL:
+                propertyType = EdmPrimitiveTypeKind.Decimal.getFullQualifiedName().getFullQualifiedNameAsString();
+                value = paramValue == null ? null : ConverterUtil.convertToBigDecimal(paramValue);
+                break;
+            case SINGLE:
+                propertyType = EdmPrimitiveTypeKind.Single.getFullQualifiedName().getFullQualifiedNameAsString();
+                value = paramValue == null ? null : ConverterUtil.convertToFloat(paramValue);
+                break;
+            case TIMEOFDAY:
+                propertyType = EdmPrimitiveTypeKind.TimeOfDay.getFullQualifiedName().getFullQualifiedNameAsString();
+                value = paramValue == null ? null : ConverterUtil.convertToTime(paramValue).getAsCalendar();
+                break;
+            case INT64:
+                propertyType = EdmPrimitiveTypeKind.Int64.getFullQualifiedName().getFullQualifiedNameAsString();
+                value = paramValue == null ? null : ConverterUtil.convertToLong(paramValue);
+                break;
+            case DATE_TIMEOFFSET:
+                propertyType = EdmPrimitiveTypeKind.DateTimeOffset.getFullQualifiedName().getFullQualifiedNameAsString();
+                value = ConverterUtil.convertToDateTime(paramValue);
+                break;
+            case GUID:
+                propertyType = EdmPrimitiveTypeKind.Guid.getFullQualifiedName().getFullQualifiedNameAsString();
+                value = UUID.fromString(paramValue);
+                break;
+            case STREAM:
+                propertyType = EdmPrimitiveTypeKind.Stream.getFullQualifiedName().getFullQualifiedNameAsString();
+                value = paramValue;
+                break;
+            case GEOGRAPHY:
+                propertyType = EdmPrimitiveTypeKind.Geography.getFullQualifiedName().getFullQualifiedNameAsString();
+                value = paramValue;
+                break;
+            case GEOGRAPHY_POINT:
+                propertyType = EdmPrimitiveTypeKind.GeographyPoint.getFullQualifiedName().getFullQualifiedNameAsString();
+                value = paramValue;
+                break;
+            case GEOGRAPHY_LINE_STRING:
+                propertyType =
+                        EdmPrimitiveTypeKind.GeographyLineString.getFullQualifiedName().getFullQualifiedNameAsString();
+                value = paramValue;
+                break;
+            case GEOGRAPHY_POLYGON:
+                propertyType = EdmPrimitiveTypeKind.GeographyPolygon.getFullQualifiedName().getFullQualifiedNameAsString();
+                value = paramValue;
+                break;
+            case GEOGRAPHY_MULTIPOINT:
+                propertyType =
+                        EdmPrimitiveTypeKind.GeographyMultiPoint.getFullQualifiedName().getFullQualifiedNameAsString();
+                value = paramValue;
+                break;
+            case GEOGRAPHY_MULTILINE_STRING:
+                propertyType =
+                        EdmPrimitiveTypeKind.GeographyMultiLineString.getFullQualifiedName().getFullQualifiedNameAsString();
+                value = paramValue;
+                break;
+            case GEOGRAPHY_MULTIPOLYGON:
+                propertyType =
+                        EdmPrimitiveTypeKind.GeographyMultiPolygon.getFullQualifiedName().getFullQualifiedNameAsString();
+                value = paramValue;
+                break;
+            case GEOGRAPHY_COLLECTION:
+                propertyType =
+                        EdmPrimitiveTypeKind.GeographyCollection.getFullQualifiedName().getFullQualifiedNameAsString();
+                value = paramValue;
+                break;
+            case GEOMETRY:
+                propertyType = EdmPrimitiveTypeKind.Geometry.getFullQualifiedName().getFullQualifiedNameAsString();
+                value = paramValue;
+                break;
+            case GEOMETRY_POINT:
+                propertyType = EdmPrimitiveTypeKind.GeometryPoint.getFullQualifiedName().getFullQualifiedNameAsString();
+                value = paramValue;
+                break;
+            case GEOMETRY_LINE_STRING:
+                propertyType =
+                        EdmPrimitiveTypeKind.GeometryLineString.getFullQualifiedName().getFullQualifiedNameAsString();
+                value = paramValue;
+                break;
+            case GEOMETRY_POLYGON:
+                propertyType = EdmPrimitiveTypeKind.GeometryPolygon.getFullQualifiedName().getFullQualifiedNameAsString();
+                value = paramValue;
+                break;
+            case GEOMETRY_MULTIPOINT:
+                propertyType =
+                        EdmPrimitiveTypeKind.GeometryMultiPoint.getFullQualifiedName().getFullQualifiedNameAsString();
+                value = paramValue;
+                break;
+            case GEOMETRY_MULTILINE_STRING:
+                propertyType =
+                        EdmPrimitiveTypeKind.GeographyMultiLineString.getFullQualifiedName().getFullQualifiedNameAsString();
+                value = paramValue;
+                break;
+            case GEOMETRY_MULTIPOLYGON:
+                propertyType =
+                        EdmPrimitiveTypeKind.GeometryMultiPolygon.getFullQualifiedName().getFullQualifiedNameAsString();
+                value = paramValue;
+                break;
+            case GEOMETRY_COLLECTION:
+                propertyType =
+                        EdmPrimitiveTypeKind.GeometryCollection.getFullQualifiedName().getFullQualifiedNameAsString();
+                value = paramValue;
+                break;
+            default:
+                propertyType = EdmPrimitiveTypeKind.String.getFullQualifiedName().getFullQualifiedNameAsString();
+                value = paramValue;
+                break;
+        }
+        return new Property(propertyType, name, ValueType.PRIMITIVE, value);
+    }
+
+    public static byte[] getBytesFromBase64String(String base64Str) throws ODataServiceFault {
+        try {
+            return Base64.decodeBase64(base64Str.getBytes(DBConstants.DEFAULT_CHAR_SET_TYPE));
+        } catch (Exception e) {
+            throw new ODataServiceFault(e.getMessage());
+        }
     }
 
 }
