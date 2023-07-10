372293cfb10506f58f52b8791c0eaac3ca688ea3
piyumaldk <piyumaldk@gmail.com>
2023-07-10 16:15:35 +0530

    Improve Choreo Statistics Publisher to handle Inbounds
    Related to https://github.com/wso2/api-manager/issues/1992

--- components/mediation/data-publishers/org.wso2.micro.integrator.analytics.messageflow.data.publisher/src/main/java/org/wso2/micro/integrator/analytics/messageflow/data/publisher/publish/choreo/ChoreoStatisticsPublisher.java
+++ components/mediation/data-publishers/org.wso2.micro.integrator.analytics.messageflow.data.publisher/src/main/java/org/wso2/micro/integrator/analytics/messageflow/data/publisher/publish/choreo/ChoreoStatisticsPublisher.java
@@ -1,6 +1,5 @@
 package org.wso2.micro.integrator.analytics.messageflow.data.publisher.publish.choreo;
 
-import com.google.gson.Gson;
 import com.google.gson.JsonObject;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
@@ -13,10 +12,13 @@ import org.apache.synapse.aspects.flow.statistics.publishing.PublishingFlow;
 import org.apache.synapse.aspects.flow.statistics.util.StatisticsConstants;
 import org.apache.synapse.commons.CorrelationConstants;
 import org.apache.synapse.config.SynapsePropertiesLoader;
+import org.apache.synapse.mediators.base.SequenceMediator;
 import org.apache.synapse.rest.RESTConstants;
 import org.wso2.micro.integrator.analytics.messageflow.data.publisher.publish.StatisticsPublisher;
 import org.wso2.micro.integrator.analytics.messageflow.data.publisher.publish.elasticsearch.ElasticConstants;
 import org.wso2.micro.integrator.initializer.ServiceBusInitializer;
+
+import java.util.Objects;
 
 public class ChoreoStatisticsPublisher implements StatisticsPublisher {
     private static ChoreoStatisticsPublisher instance = null;
@@ -78,29 +80,87 @@ public class ChoreoStatisticsPublisher implements StatisticsPublisher {
     }
 
     private void publishAnalytics(PublishingEvent event) {
+        JsonObject payload = choreoPayload(event);
+    }
+
+    private JsonObject choreoPayload (PublishingEvent event) {
+        try {
+            ElasticMetadata metadata = event.getElasticMetadata();
+            JsonObject pL = new JsonObject();
+            String type = event.getComponentType();
+            ServerConfigurationInformation config = ServiceBusInitializer.getConfigurationInformation();
+
+            // Server related information
+            pL.addProperty("serverHostname", (config != null) ? config.getHostName() : null);
+            pL.addProperty("serverId", SynapsePropertiesLoader.getPropertyValue(
+                    ElasticConstants.SynapseConfigKeys.IDENTIFIER, pL.get("serverHostname").getAsString()));
+            pL.addProperty("serverName", (config != null) ? config.getServerName() : null);
+            pL.addProperty("serverIpAddress", (config != null) ? config.getIpAddress() : null);
+
+            // Message related information
+            // All artifact related messages should contain below information
+            pL.addProperty("name", getName(type, event));
+            pL.addProperty("timestamp", event.getStartTime());
+            pL.addProperty("entityType", event.getComponentType());
+            pL.addProperty("faultResponse", metadata.isFaultResponse());
+            pL.addProperty("failure", event.getFaultCount() != 0);
+            pL.addProperty("latency", event.getDuration());
+            pL.addProperty("messageId", metadata.getMessageId());
+
+            if (Objects.equals(type, "API")) {
+                // Only APIs have below 4 information
+                pL.addProperty("apiSubRequestPath", metadata.getProperty(RESTConstants.REST_SUB_REQUEST_PATH).toString());
+                pL.addProperty("apiMethod", metadata.getProperty(RESTConstants.REST_METHOD).toString());
+                pL.addProperty("apiContext", metadata.getProperty(RESTConstants.REST_API_CONTEXT).toString());
+                pL.addProperty("apiTransport", metadata.getProperty(SynapseConstants.TRANSPORT_IN_NAME).toString());
+            } else {
+                // Setting N/A because pL (Payload) is required to be common for all 5 artifacts.
+                pL.addProperty("apiSubRequestPath", "N/A");
+                pL.addProperty("apiMethod", "N/A");
+                pL.addProperty("apiContext", "N/A");
+                pL.addProperty("apiTransport", "N/A");
+            }
+
+            if (metadata.getProperty(CorrelationConstants.CORRELATION_ID) == null &&
+                    Boolean.parseBoolean(metadata.getProperty("isInbound").toString())) {
+                // Correlation ID is not available for inbound messages. Others should have it.
+                // If correlation ID is not available, second condition will verify that it is an inbound message.
+                // isInbound is only available for inbound messages.
+                pL.addProperty("correlationId", "N/A");
+            } else {
+                pL.addProperty("correlationId", metadata.getProperty(CorrelationConstants.CORRELATION_ID).toString());
+            }
+            System.out.println(pL);
+            return pL;
+        } catch (Exception e) {
+            log.error("Error while collecting information for Choreo Statistics Payload. Exception : ", e);
+            return null;
+        }
+    }
+
+    /**
+     * Get the name of the artifact.
+     *
+     * This is to get the name of the artifact from the Publishing flow.
+     *
+     * @param type This is the type of the artifact.
+     * @param event This is the event published by synapse.
+     * @return This returns a String which contains the artifact name.
+     */
+    private String getName(String type, PublishingEvent event) {
         ElasticMetadata metadata = event.getElasticMetadata();
-        JsonObject pL = new JsonObject();
-
-        ServerConfigurationInformation config = ServiceBusInitializer.getConfigurationInformation();
-
-        pL.addProperty("serverHostname", (config != null) ? config.getHostName() : null);
-        pL.addProperty("serverId", SynapsePropertiesLoader.getPropertyValue(
-                ElasticConstants.SynapseConfigKeys.IDENTIFIER, pL.get("serverHostname").getAsString()));
-        pL.addProperty("serverName", (config != null) ? config.getServerName() : null);
-        pL.addProperty("serverIpAddress", (config != null) ? config.getIpAddress() : null);
-        pL.addProperty("name", metadata.getProperty(RESTConstants.SYNAPSE_REST_API).toString());
-        pL.addProperty("apiSubRequestPath", metadata.getProperty(RESTConstants.REST_SUB_REQUEST_PATH).toString());
-        pL.addProperty("apiMethod", metadata.getProperty(RESTConstants.REST_METHOD).toString());
-        pL.addProperty("apiContext", metadata.getProperty(RESTConstants.REST_API_CONTEXT).toString());
-        pL.addProperty("apiTransport", metadata.getProperty(SynapseConstants.TRANSPORT_IN_NAME).toString());
-        pL.addProperty("timestamp", event.getStartTime());
-        pL.addProperty("entityType", event.getComponentType());
-        pL.addProperty("faultResponse", metadata.isFaultResponse());
-        pL.addProperty("failure", event.getFaultCount() != 0);
-        pL.addProperty("latency", event.getDuration());
-        pL.addProperty("correlationId", metadata.getProperty(CorrelationConstants.CORRELATION_ID).toString());
-        pL.addProperty("messageId", metadata.getMessageId());
-
-        System.out.println(pL);
+        switch (type) {
+            case "API":
+                return metadata.getProperty(RESTConstants.SYNAPSE_REST_API).toString();
+            case "Endpoint":
+                return metadata.getEndpoint(event.getComponentName()).getName();
+            case "ProxyService":
+            case "InboundEndpoint":
+                return event.getComponentName();
+            case "SequenceMediator":
+                return event.getElasticMetadata().getSequence(event.getComponentName()).getName();
+            default:
+                return null;
+        }
     }
 }
