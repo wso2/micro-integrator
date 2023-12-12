a5b4ea02957adabb603f4203a8ef350edd565896
Arunan Sugunakumar <arunans23@gmail.com>
2023-12-12 10:18:04 +0530

    Add web socket transport sender hostname verification

--- components/mediation/transports/org.wso2.micro.integrator.websocket.transport/src/main/java/org/wso2/micro/integrator/websocket/transport/WebsocketConstants.java
+++ components/mediation/transports/org.wso2.micro.integrator.websocket.transport/src/main/java/org/wso2/micro/integrator/websocket/transport/WebsocketConstants.java
@@ -53,6 +53,7 @@ public class WebsocketConstants {
 
     public static final String WEBSOCKET_CUSTOM_HEADER_PREFIX = "websocket.custom.header.";
     public static final String WEBSOCKET_CUSTOM_HEADER_CONFIG = "ws.custom.header";
+    public static final String WEBSOCKET_HOSTNAME_VERIFICATION_CONFIG = "ws.client.enable.hostname.verification";
 
     public static final String CONNECTION_TERMINATE = "connection.terminate";
 
