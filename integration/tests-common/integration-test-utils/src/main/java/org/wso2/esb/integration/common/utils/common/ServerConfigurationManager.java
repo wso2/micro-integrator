6516006574db4b964bcb25b15385795c13f6b39a
Kalaiyarasiganeshalingam <ashakalai31@gmail.com>
2024-11-08 11:29:18 +0530

    Revert the changes

--- integration/tests-common/integration-test-utils/src/main/java/org/wso2/esb/integration/common/utils/common/ServerConfigurationManager.java
+++ integration/tests-common/integration-test-utils/src/main/java/org/wso2/esb/integration/common/utils/common/ServerConfigurationManager.java
@@ -45,7 +45,6 @@ import java.nio.file.Paths;
 import java.nio.file.StandardCopyOption;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Map;
 import java.util.Properties;
 import java.util.concurrent.TimeUnit;
 import java.util.function.BooleanSupplier;
